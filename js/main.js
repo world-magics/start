f01f00e7894479584adc8eb489540cc6db22a99c
xray-ray <sherzodyarashev1997@gmail.com>
2022-01-07 16:46:24 +0500

    Add container for slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function(){
+    $('.header__slider').slick({
+        dots:true,
+        prevArrow:'<button type="button" class="slick-prev"><img src="images/next.svg" alt=""></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="images/previos.svg" alt=""></button>',
+        fade:true
+    });
+
+
+
+
 
 });

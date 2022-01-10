3d92dd135e8960f0b50ccf93344b6bba4c56c694
xray-ray <sherzodyarashev1997@gmail.com>
2022-01-10 17:22:37 +0500

    slider install

--- js/main.js
+++ js/main.js
@@ -11,13 +11,16 @@ $(function(){
         slidesToScroll: 1,       
          focusOnSelect: true,       
         asNavFor: '.product__content',
-        vertical:true
-      });
+        vertical: true,
+        prevArrow:'<button type="button" class="product-prev"><img src="images/product_next.png" alt=""></button>',
+        nextArrow:'<button type="button" class="product-next"><img src="images/product_prev.png" alt=""></button>',
+      }); 
       $('.product__content').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         asNavFor: '.product__name',
         fade: true,
+        arrow:false,
       });
 //End second slider for header next
 

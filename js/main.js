210e61059e96339e40fb7e7c8d9f6ebbc5d2d73e
xray-ray <sherzodyarashev1997@gmail.com>
2022-01-10 10:16:38 +0500

    slick

--- js/main.js
+++ js/main.js
@@ -5,9 +5,21 @@ $(function(){
         nextArrow:'<button type="button" class="slick-next"><img src="images/previos.svg" alt=""></button>',
         fade:true
     });
-
-
-
+// second slider for header next
+    $('.product__name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,       
+         focusOnSelect: true,       
+        asNavFor: '.product__content',
+        vertical:true
+      });
+      $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        fade: true,
+      });
+//End second slider for header next
 
 
 });
